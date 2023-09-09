d472885496c2ae0cc06abb92fc513258d4d3e378
Tanuththiga <anuththiga25jt@gmail.com>
2023-09-09 14:42:27 +0800

    api integration

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [image, setImage] = useState('')
+
+  const handleClick = () => {
+    axios.get('https://api.generated.photos/api/v1/faces?api_key=API_KEY&order_by=random')
+    .then(res=> {
+      const photo = res.data.faces[0].urls[4][512];
+      photo && setImage(photo);
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>AI Photo Generator</h1>
+      {image && <img src={image} alt='AI Image'/>}
+      <button onClick={handleClick}>New Image</button>
     </div>
   );
 }
